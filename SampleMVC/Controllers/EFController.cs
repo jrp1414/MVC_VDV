7a1b0fedca8ee1f77b02c5827ef094f3c732f20e
Ranjan Panda <jyotiranjan.p14@gmail.com>
2017-11-20 23:00:20 +0530

    20 November Code

--- SampleMVC/Controllers/EFController.cs
+++ SampleMVC/Controllers/EFController.cs
@@ -9,13 +9,77 @@ namespace SampleMVC.Controllers
 {
     public class EFController : Controller
     {
+        private ERPSystemEntities dbContext;
+        public EFController()
+        {
+            dbContext = new ERPSystemEntities();
+        }
         // GET: EF
         [Route("Students")]
         public ActionResult StudentsList()
         {
-            var dbContext = new ERPSystemEntities();
             var students = dbContext.StudentMasters.ToList(); // Select * from StudentMasters
             return View(students);
         }
+
+        [Route("AddStudent")]
+        [HttpGet]
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+
+        [Route("AddStudent")]
+        [HttpPost]
+        public ActionResult AddStudent(StudentMaster data)
+        {
+            if (ModelState.IsValid)
+            {
+                dbContext.StudentMasters.Add(data);
+                dbContext.SaveChanges();
+                return RedirectToAction("StudentsList");
+            }
+            return View(data);
+        }
+
+        //[Route("EditStudent/{id=0}")]
+        [HttpGet]
+        public ActionResult EditStudent(int id=0)
+        {
+            var student = dbContext.StudentMasters.SingleOrDefault(m => m.Id == id);
+            if (student!=null)
+            {
+                //student.IsAdult = 
+                return View(student);
+            }
+            return RedirectToAction("StudentsList");
+        }
+
+        //[Route("EditStudent")]
+        [HttpPost]
+        public ActionResult EditStudent(StudentMaster data)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = dbContext.StudentMasters.SingleOrDefault(m=>m.Id==data.Id);
+                student.Name = data.Name;
+                student.Address = data.Address;
+                student.Age = data.Age;
+                //student.IsAdult = data.Age >= 18 ? "Y" : "N";
+                //student.IsAdult = data.Age >= 18 ? "Y" : "N";
+                student.MobileNo = data.MobileNo;
+                dbContext.SaveChanges();
+                return RedirectToAction("StudentsList");
+            }
+            return View(data);
+        }
+
+        public ActionResult DeleteStudent(int id = 0)
+        {
+            var student = dbContext.StudentMasters
+            
+            return RedirectToAction("StudentsList");
+        }
+        
     }
 }
