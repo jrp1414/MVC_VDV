8b2e046d1286c7ad2c6dcadb979bb9b29637f20d
Ranjan Panda <jyotiranjan.p14@gmail.com>
2017-12-10 22:54:59 +0530

    Javascript and jQuery Intro 10th December Code

---
+++ SampleMVC/Controllers/JqController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public class JqController : Controller
+    {
+        // GET: Jq
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult JqEx1()
+        {
+            return View();
+        }
+    }
+}
